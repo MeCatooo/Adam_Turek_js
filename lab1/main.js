f002138aea0d8f030d541f36be92399c1dc078fe
MeCatooo <79982959+MeCatooo@users.noreply.github.com>
2022-10-06 09:43:58 +0200

    avg and sum

--- lab1/main.js
+++ lab1/main.js
@@ -2,9 +2,12 @@ var submitButton = document.querySelector('#inputs')
 submitButton.addEventListener("change", () => {
     var data = document.querySelectorAll("input");
     var castedData = []
-
     data.forEach(x => castedData.push(+x.value ?? 0))
+    var sum = castedData.reduce((a, b) => a + b, 0)
 
+
+    document.querySelector("#sum").textContent = sum
+    document.querySelector("#avg").textContent = sum/castedData.length
     document.querySelector("#min").textContent = Math.min.apply(Math, castedData)
     document.querySelector("#max").textContent = Math.max.apply(Math, castedData)
 })
