9c7b9a131fa430f326227672568a13468d30268a
MeCatooo <adam.turek@microsoft.wsei.edu.pl>
2022-11-02 15:15:43 +0100

    Drum kit

--- lab3/main.js
+++ lab3/main.js
@@ -1,8 +1,9 @@
 recording = {
     isOn: false,
-    channel: 1
+    channel: []
 };
 let data = [];
+let intervals = [];
 
 document.addEventListener("keypress", onKeyPress)
 
@@ -50,26 +51,62 @@ function recordingLayer(sound) {
 }
 
 function playSound(sound) {
-    const audioTag = document.querySelector(`#${sound}`);
+    const audioTag = document.querySelector(`#${sound}`).querySelector("audio");
     audioTag.currentTime = 0;
     audioTag.play();
 }
 
-document.querySelector("#start").addEventListener("click", () => {
-    if (!document.querySelector("#start").checked)
-        data = [];
-    else {
-        recording = {
-            isOn: true,
-            channel: 1
-        };
+function removeChannel(number) {
+    const index = recording.channel.indexOf(number);
+    if (index > -1) {
+        recording.channel.splice(index, 1);
     }
-})
-document.querySelector("#play").addEventListener("click", () => {
-    for(let i = 0; i < data.length; i++) {
-        setTimeout(() => {
-            playSound(data[i].soundName)
-        }, data[i].timestamp - data[0].timestamp)
-    }
+}
 
+function removeInterval(id) {
+    const index = intervals.findIndex(item => item.id === id)
+    if (index > -1) {
+        intervals.splice(index, 1);
+    }
+}
+
+document.querySelectorAll("#start").addEventListener("click", (event) => {
+    if (event.target.checked) {
+        data = [];
+        recording.isOn = true;
+        recording.channel.push(event.target.parentElement.id);
+    }
+    else {
+        recording.isOn = false;
+        removeChannel(event.target.parentElement.id)
+    }
 })
+
+document.querySelectorAll("#play").forEach(item =>{
+    item.addEventListener("click", () => {
+        for (let i = 0; i < data.length; i++) {
+            setTimeout(() => {
+                playSound(data[i].soundName)
+            }, data[i].timestamp - data[0].timestamp)
+        }
+    })
+})
+
+document.querySelectorAll(".audio-style input").forEach(element => {
+    element.addEventListener("change", (event) => {
+        const id = event.target.parentElement.id;
+        const isRunning = intervals.find(item => item.id === id);
+        if (isRunning) {
+            clearInterval(isRunning.interval)
+            removeInterval(id);
+        }
+        if(event.target.value <= 0)
+            return;
+        const interval = setInterval(() => {
+            recordingLayer(id)
+        }, 1/(event.target.value / 60) * 200)
+        const toSave = { interval: interval, id: id }
+        intervals.push(toSave)
+    })
+});
+
